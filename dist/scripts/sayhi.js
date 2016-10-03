b0c01d7ddebef5325d3eb5949fb9d5a265e9397a
Goden <goden.incredible@gmail.com>
2016-10-04 01:11:34 +0800

    增加自有屬性

--- dist/scripts/sayhi.js
+++ dist/scripts/sayhi.js
@@ -1,12 +1,16 @@
-var Hi = React.createClass({displayName: "Hi",
-  render: function() {
-    return React.createElement("h1", null, "Hi!");
+'use strict';
+
+var props = {
+  style: 'width:200px',
+  classNanme: 'main',
+  value: 'yo'
+};
+
+var Hi = React.createClass({ displayName: "Hi",
+  render: function render() {
+    return React.createElement("h1", { "data-attr": "yes" }, "Hi!");
   }
 });
 
-ReactDOM.render(
-  React.createElement(Hi, null),
-  document.getElementById('example2')
-);
-
+ReactDOM.render(React.createElement(Hi, null), document.getElementById('example2'));
 //# sourceMappingURL=sayhi.js.map
--- src/sayhi.jsx
+++ src/sayhi.jsx
@@ -1,10 +1,16 @@
+var props = {
+  style: 'width:200px',
+  classNanme: 'main',
+  value: 'yo'
+};
+
 var Hi = React.createClass({
   render: function() {
-    return <h1>Hi!</h1>;
+    return <h1 data-attr="yes">Hi!</h1>;
   }
 });
 
 ReactDOM.render(
-  <Hi/>,
+  <Hi />,
   document.getElementById('example2')
 );

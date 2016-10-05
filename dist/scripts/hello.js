8c91edeff04b5a6708ab9e1b6c6a9f55a7ebc0b4
Goden <goden.incredible@gmail.com>
2016-10-06 00:55:39 +0800

    重新調整檔案結構使其合乎reactjs的開發方式

--- dist/scripts/hello.js
+++
@@ -1,8 +0,0 @@
-var msg = 'Hello, world!!';
-
-ReactDOM.render(
-    React.createElement("h1", null, msg),
-    document.getElementById('example')
-);
-
-//# sourceMappingURL=hello.js.map
--- src/hello.jsx
+++
@@ -1,6 +0,0 @@
-var msg = 'Hello, world!!';
-
-ReactDOM.render(
-    <h1>{msg}</h1>,
-    document.getElementById('example')
-);
